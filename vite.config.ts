77d5b0add0d2475f2311923daa6cd73ab67c58f3
Nihal Gonsalves <nihal@nihalgonsalves.com>
2024-10-26 17:45:41 +0200

    chore: restyle with tailwind (#5)

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+	server: {
+		hmr: {
+			port: 3001,
+		},
+	},
 	build: {
 		minify: "esbuild",
 		cssMinify: "lightningcss",
