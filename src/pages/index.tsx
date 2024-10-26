e562d115585551669862574fd58459cbcf277f42
Nihal Gonsalves <nihal@nihalgonsalves.com>
2024-10-26 21:23:39 +0200

    feat: add dark mode bg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,70 +8,68 @@ import {
 
 export default async function HomePage() {
 	return (
-		<div className="h-full lg:py-8">
-			<div className="h-full bg-sky-950 text-gray-200 lg:h-auto">
-				<div className="flex flex-col gap-8 p-8 lg:flex-row lg:items-center">
-					<img
-						className="max-w-48 rounded-full"
-						alt="Profile"
-						src="/images/profile.webp"
-					/>
-					<main className="relative bottom-2 flex flex-col gap-4">
-						<h1 className="text-4xl">Nihal Gonsalves</h1>
-						<section>
-							<h2 className="text-lg">Senior Software Engineer</h2>
-							<div className="flex items-center gap-2">
-								<MapPinHouseIcon aria-label="Location" className="size-5" />
-								Berlin, Germany
-							</div>
-						</section>
-						<section className="flex flex-col gap-2 lg:flex-row lg:gap-4">
-							<a
-								href="mailto:me@nihalgonsalv.es"
-								className="flex items-center gap-2 underline hover:no-underline"
-							>
-								<MailIcon
-									className="relative top-[1px] size-5 shrink-0 text-white"
-									aria-label="Email"
-								/>
-								me@nihalgonsalv.es
-							</a>
+		<div className="h-full lg:py-8 dark:bg-slate-950">
+			<div className="flex h-full flex-col gap-8 bg-sky-950 p-8 text-gray-200 lg:h-auto lg:flex-row lg:items-center">
+				<img
+					className="max-w-48 rounded-full"
+					alt="Profile"
+					src="/images/profile.webp"
+				/>
+				<main className="relative bottom-2 flex flex-col gap-4">
+					<h1 className="text-4xl">Nihal Gonsalves</h1>
+					<section>
+						<h2 className="text-lg">Senior Software Engineer</h2>
+						<div className="flex items-center gap-2">
+							<MapPinHouseIcon aria-label="Location" className="size-5" />
+							Berlin, Germany
+						</div>
+					</section>
+					<section className="flex flex-col gap-2 lg:flex-row lg:gap-4">
+						<a
+							href="mailto:me@nihalgonsalv.es"
+							className="flex items-center gap-2 underline hover:no-underline"
+						>
+							<MailIcon
+								className="relative top-[1px] size-5 shrink-0 text-white"
+								aria-label="Email"
+							/>
+							me@nihalgonsalv.es
+						</a>
 
-							<a
-								href="https://www.linkedin.com/in/nihal-gonsalves"
-								className="flex items-center gap-2 underline hover:no-underline"
-							>
-								<LinkedinIcon
-									className="size-5 shrink-0 text-white"
-									aria-label="LinkedIn"
-								/>
-								Nihal Gonsalves
-							</a>
+						<a
+							href="https://www.linkedin.com/in/nihal-gonsalves"
+							className="flex items-center gap-2 underline hover:no-underline"
+						>
+							<LinkedinIcon
+								className="size-5 shrink-0 text-white"
+								aria-label="LinkedIn"
+							/>
+							Nihal Gonsalves
+						</a>
 
-							<a
-								href="https://github.com/nihalgonsalves"
-								className="flex items-center gap-2 underline hover:no-underline"
-							>
-								<GithubIcon
-									className="relative top-[1px] size-5 shrink-0 text-white"
-									aria-label="GitHub"
-								/>
-								nihalgonsalves
-							</a>
+						<a
+							href="https://github.com/nihalgonsalves"
+							className="flex items-center gap-2 underline hover:no-underline"
+						>
+							<GithubIcon
+								className="relative top-[1px] size-5 shrink-0 text-white"
+								aria-label="GitHub"
+							/>
+							nihalgonsalves
+						</a>
 
-							<a
-								href="https://www.instagram.com/nihalgonsalves"
-								className="flex items-center gap-2 underline hover:no-underline"
-							>
-								<InstagramIcon
-									className="relative top-[1px] size-5 shrink-0 text-white"
-									aria-label="Instagram"
-								/>
-								nihalgonsalves
-							</a>
-						</section>
-					</main>
-				</div>
+						<a
+							href="https://www.instagram.com/nihalgonsalves"
+							className="flex items-center gap-2 underline hover:no-underline"
+						>
+							<InstagramIcon
+								className="relative top-[1px] size-5 shrink-0 text-white"
+								aria-label="Instagram"
+							/>
+							nihalgonsalves
+						</a>
+					</section>
+				</main>
 			</div>
 		</div>
 	);
