6ca92fd13df551c9421550e8ea04f1e3dbbcb1e1
Nihal Gonsalves <nihal@nihalgonsalves.com>
2024-12-06 15:43:32 +0100

    chore: use released react 19 versions

--- src/pages/_layout.tsx
+++ src/pages/_layout.tsx
@@ -1,23 +1,10 @@
-import "../styles.css";
-
 import type { ReactNode } from "react";
 
+import "../styles.css";
 type RootLayoutProps = { children: ReactNode };
 
 export default async function RootLayout({ children }: RootLayoutProps) {
-	return (
-		<html lang="en">
-			<head>
-				<title>Nihal Gonsalves</title>
-				<meta charSet="utf-8" />
-				<meta content="Nihal Gonsalves" name="author" />
-				<meta name="description" content="Software Engineer in Berlin, DE" />
-				<meta content="width=device-width, initial-scale=1" name="viewport" />
-				<link rel="preconnect" href="https://rsms.me/" />
-			</head>
-			<body>{children}</body>
-		</html>
-	);
+	return children;
 }
 
 export const getConfig = async () => {
---
+++ src/pages/_root.tsx
@@ -0,0 +1,25 @@
+import type { ReactNode } from "react";
+
+type RootElementProps = { children: ReactNode };
+
+export default async function RootElement({ children }: RootElementProps) {
+	return (
+		<html lang="en">
+			<head>
+				<title>Nihal Gonsalves</title>
+				<meta charSet="utf-8" />
+				<meta content="Nihal Gonsalves" name="author" />
+				<meta name="description" content="Software Engineer in Berlin, DE" />
+				<meta content="width=device-width, initial-scale=1" name="viewport" />
+				<link rel="preconnect" href="https://rsms.me/" />
+			</head>
+			<body>{children}</body>
+		</html>
+	);
+}
+
+export const getConfig = async () => {
+	return {
+		render: "static",
+	};
+};
