821e4f51ea6bfc4866dbd25bc7bda7c01bc4d6a4
Nihal Gonsalves <nihal@nihalgonsalves.com>
2025-02-19 22:35:59 +0100

    revert(chore): add debug route to inspect cf build
    This reverts commit 64976705a8e06090ef19db1b0a5e1a35832f5845.

--- src/pages/debug.tsx
+++
@@ -1,9 +0,0 @@
-export default async function DebugComponent() {
-	return <pre>{JSON.stringify(process.env, null, 2)}</pre>;
-}
-
-export const getConfig = async () => {
-	return {
-		render: "static",
-	};
-};
