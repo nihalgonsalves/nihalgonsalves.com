891baa703da7ea5c46225680034abbc5725c476d
Nihal Gonsalves <nihal@nihalgonsalves.com>
2025-07-14 21:25:02 +0200

    security: add CSP header

--- src/pages/_root.tsx
+++ src/pages/_root.tsx
@@ -2,10 +2,40 @@ import type { ReactNode } from "react";
 
 type RootElementProps = { children: ReactNode };
 
+const getCsp = () => {
+	const config: [
+		string,
+		("'self'" | "'unsafe-inline'" | `https://${string}` | "*")[],
+	][] = [
+		["default-src", ["'self'"]],
+		["script-src", ["'self'", "'unsafe-inline'"]],
+		[
+			"style-src",
+			[
+				"'self'",
+				...(import.meta.env.DEV ? ["'unsafe-inline'" as const] : []),
+				"https://fonts.googleapis.com/css2",
+			],
+		],
+		["font-src", ["'self'", "https://fonts.gstatic.com"]],
+	];
+
+	if (import.meta.env.DEV) {
+		config.push(["connect-src", ["*"]]);
+	}
+
+	return config
+		.map(([directive, sources]) => {
+			return `${directive} ${sources.join(" ")}`;
+		})
+		.join("; ");
+};
+
 export default async function RootElement({ children }: RootElementProps) {
 	return (
 		<html lang="en">
 			<head>
+				<meta httpEquiv="Content-Security-Policy" content={getCsp()} />
 				<title>Nihal Gonsalves</title>
 				<meta charSet="utf-8" />
 				<meta content="Nihal Gonsalves" name="author" />
